8d02866c017cd4f1522416b9e6eb4519f30a44b0
antonjlin <antonjlin@gmail.com>
2019-08-10 22:49:01 -0700

    discord hooks

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const rp = require('request-promise')
 var indexRouter = require('./routes/index');
 
 var app = express();
@@ -16,4 +16,15 @@ app.use('/assets',express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
+app.get('/apply',(req, res) => {
+    if(process.env.discord_hook){
+        rp({
+            method:'POST',
+            url:process.env.discord_hook,
+            json:true,
+            body:{"username": "test", "content": `${new Date().toLocaleString()} Someone viewed the application`}
+        })
+    }
+    res.redirect('https://docs.google.com/forms/d/e/1FAIpQLSe5i1Q4RBlnA4aUssX-qIW44asIrHvVSnEIFqfzbjNLw0awJg/viewform')
+})
 module.exports = app;
