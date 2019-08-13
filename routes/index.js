6878a8dbf2051c9dcacca84ea370f42fbdeac75b
Karen Li <karennli49@gmail.com>
2019-08-13 15:35:33 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/apply', function(req, res, next) {
   res.sendFile(path.join(__dirname,'../public/apply.html'));
 });
 
-router.get('/sponsorships.pdf', function(req, res, next) {
+router.get('/sponsorship.pdf', function(req, res, next) {
   res.sendFile(path.join(__dirname,'../public/sponsorship.pdf'));
 });
 
