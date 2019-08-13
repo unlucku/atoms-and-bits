c09a7b262c0ad982240dcaf2b1dacd01168ba84b
Karen Li <karennli49@gmail.com>
2019-08-13 15:34:10 -0700

    sponsorship packet

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ router.get('/apply', function(req, res, next) {
   res.sendFile(path.join(__dirname,'../public/apply.html'));
 });
 
+router.get('/sponsorships.pdf', function(req, res, next) {
+  res.sendFile(path.join(__dirname,'../public/sponsorship.pdf'));
+});
+
+res.sendFile(path.join(__dirname,'../public/sponsorship.pdf'));
+});
 module.exports = router;
